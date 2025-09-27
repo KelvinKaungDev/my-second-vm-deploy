9942641761e35fc667039c4c499a880bae87a75f
Kelvin Kaung <107702120+KelvinKaungDev@users.noreply.github.com>
2025-09-27 16:10:30 +0700

    Deployed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/app/next-vat-app',
+  trailingSlash: true,
 };
 
 export default nextConfig;
